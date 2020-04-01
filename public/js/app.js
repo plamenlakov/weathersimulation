0f613e198720ee3fe06314a89439543ddbf45a6a
Plamen Z <plamenzaykov99@gmail.com>
2020-04-01 21:17:43 +0200

    merging

--- public/js/app.js
+++ public/js/app.js
@@ -108009,8 +108009,8 @@ function Piechart() {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Bobby\Desktop\procp\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\Bobby\Desktop\procp\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! D:\Fontys\ProCP\procp\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! D:\Fontys\ProCP\procp\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
