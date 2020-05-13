07fa7b7acd93d61afc7baa4c28c601533ff33164
BobbyPC <borislav.kiselkov@gmail.com>
2020-05-13 22:07:30 +0200

    outline borders added

--- resources/js/components/Map.js
+++ resources/js/components/Map.js
@@ -14,7 +14,7 @@ class Map extends React.Component {
     this.state = {
       data: null
     }
-    
+
   }
 
   componentDidMount() {
@@ -75,7 +75,7 @@ class Map extends React.Component {
       controls.enableDamping = true;
       controls.dampingFactor = 1;
       controls.rotateSpeed = .8;
-      controls.maxPolarAngle = Math.PI / 2 - .3;
+      //controls.maxPolarAngle = Math.PI / 2 - .3;
 
       //initialize plane
       plane = new THREE.PlaneBufferGeometry(2500 * SIZE_AMPLIFIER, 2500 * SIZE_AMPLIFIER, 999, 999);
@@ -154,40 +154,21 @@ class Map extends React.Component {
 
       scene.add(mesh);
 
-      var self = this;
-      
-      const width1 = 1500 * SIZE_AMPLIFIER,
-        height1 = 1500 * SIZE_AMPLIFIER,
-        projection = d3.geoMercator()
-          .center([13, 52]) //longitude, latitude
-          .translate([width1 / 2, height1 / 2]) // center the image
-          .scale([width1 / 1.5]);
+      const width1 = plane.parameters.width,//2500 * SIZE_AMPLIFIER,
+        //height1 = 3400 * SIZE_AMPLIFIER,
+        projection = d3.geoTransverseMercator().rotate([-10, 0, 0])
+
+          .center([10, 52]) //longitude, latitude
+          //.translate([width1 / 2, height1 / 2]) // center the image
+          .scale(width1 * 1.55);
       const path = d3.geoPath().projection(projection);
       const svg = d3.select("#Map").append("svg")
-        .attr("width", '100%')
-        .attr("height", '100%')
-        .attr("viewBox", "0 0 " + width1 + " " + height1)
-        .attr("preserveAspectRatio", "xMinYMin");
+        // .attr("width", '100%')
+        // .attr("height", '100%')
 
-      const zoom = d3.zoom()
-        .scaleExtent([1, 8])
-        .on('zoom', zoomed);
-
-      const g = svg.append('svg');
-
-      svg.call(zoom);
-
-      function zoomed() {
-        svg
-          .selectAll('path') // To prevent stroke width from scaling
-          .attr('transform', d3.event.transform);
-      }
-
-      d3.json('/Europe.geo.json')
+      d3.json('/Europe1.geo.json')
         .then(topology => {
 
-          var points = [];
-          
           //console.log(topology);
           svg.selectAll(".country")
             .data(topology.features)
@@ -195,80 +176,103 @@ class Map extends React.Component {
             .append("path")
             .attr("class", ".country")
             .attr("d", path)
-            .attr("fill", "#cccccc")
-            .attr("stroke", "#000000")
-            .attr("stroke-width", "3")
-            .on("mouseover", function (d) {
-              //var countryNames = self.props.data.map(d => d['name']);
-              //var hoveredCountryName = countryNames.find(c => c == d.properties.name);
-              // for (let i = 0; i < self.props.data.length; i++) {
-              //   if (self.props.data[i].name == hoveredCountryName) {
-              //     d3.select(this)
-              //       .attr("fill", "darkgrey")
-              //       .style("cursor", "pointer");
-              //   }
-    
-              // }
-            })
-            .on("click", function (d) {
-               document.getElementById("countryName").innerHTML = `<b>${d.properties.name}</b>`;
-              // var countryNames = self.props.data.map(d => d['name']);
-               var clickedCountryName = null;//countryNames.find(c => c == d.properties.name);
-              // var countryPopulation = NaN;
-              // var countryForest = NaN;
-              // var countryArea = NaN;
-              // var countryPPM = NaN;
-              // for (let i = 0; i < self.props.data.length; i++) {
-              //   if (self.props.data[i].name == clickedCountryName) {
-              //     countryPopulation = self.props.data[i].population;
-              //     countryForest = self.props.data[i].forests;
-              //     countryArea = self.props.data[i].area;
-              //     countryPPM = self.props.data[i].ppm;
-              //   }
-              // }
-              if (clickedCountryName == null) {
-                document.getElementById("modal-body").innerHTML = `Not used!`;
-                document.getElementById("modalButton").click();
-              } else {
-                document.getElementById("modal-body").innerHTML = `<h5>Population: ${countryPopulation}</h5>
-                                                                                    <h5>Forest: ${countryForest} %</h5>
-                                                                                    <h5>Area: ${countryArea}km2</h5>
-                                                                                    <h5>PPM: ${countryPPM}</h5>`;
-                document.getElementById("modalButton").click();
-              }
-    
-            })
-            .on("mouseout", function (d) {
-              d3.select(this).attr("fill", "#cccccc")
-    
-            });
+          // .attr("fill", "#cccccc")
+          // .attr("stroke", "#000000")
+          // .attr("stroke-width", "2")
+          // .on("mouseover", function (d) {
+          //   //var countryNames = self.props.data.map(d => d['name']);
+          //   //var hoveredCountryName = countryNames.find(c => c == d.properties.name);
+          //   // for (let i = 0; i < self.props.data.length; i++) {
+          //   //   if (self.props.data[i].name == hoveredCountryName) {
+          //   //     d3.select(this)
+          //   //       .attr("fill", "darkgrey")
+          //   //       .style("cursor", "pointer");
+          //   //   }
+
+          //   // }
+          // })
+          // .on("click", function (d) {
+          //    document.getElementById("countryName").innerHTML = `<b>${d.properties.name}</b>`;
+          //   // var countryNames = self.props.data.map(d => d['name']);
+          //    var clickedCountryName = null;
+          //   //countryNames.find(c => c == d.properties.name);
+          //   // var countryPopulation = NaN;
+          //   // var countryForest = NaN;
+          //   // var countryArea = NaN;
+          //   // var countryPPM = NaN;
+          //   // for (let i = 0; i < self.props.data.length; i++) {
+          //   //   if (self.props.data[i].name == clickedCountryName) {
+          //   //     countryPopulation = self.props.data[i].population;
+          //   //     countryForest = self.props.data[i].forests;
+          //   //     countryArea = self.props.data[i].area;
+          //   //     countryPPM = self.props.data[i].ppm;
+          //   //   }
+          //   // }
+          //   if (clickedCountryName == null) {
+          //     document.getElementById("modal-body").innerHTML = `Not used!`;
+          //     document.getElementById("modalButton").click();
+          //   } else {
+          //     document.getElementById("modal-body").innerHTML = `<h5>Population: ${countryPopulation}</h5>
+          //                                                                         <h5>Forest: ${countryForest} %</h5>
+          //                                                                         <h5>Area: ${countryArea}km2</h5>
+          //                                                                         <h5>PPM: ${countryPPM}</h5>`;
+          //     document.getElementById("modalButton").click();
+          //   }
+
+          // })
+          // .on("mouseout", function (d) {
+          //   d3.select(this).attr("fill", "#cccccc")
+
+          // });
 
           var svgMarkup = svg.node().outerHTML;
           var loader = new SVGLoader();
           var svgData = loader.parse(svgMarkup);
 
           var svgGroup = new THREE.Group();
-          var materialSVG = new THREE.MeshNormalMaterial();
-
-          svgData.paths.forEach((path, i)=> {
+          svgData.paths.forEach((path, i) => {
             var shapes = path.toShapes(true);
-
+            
+            
             shapes.forEach((shape, j) => {
+
               var geomSVG = new THREE.ExtrudeBufferGeometry(shape, {
-                depth: 1,
+                depth: 10,
                 bevelEnabled: false
               })
+              // var materialSVG = new THREE.MeshLambertMaterial({
+              //   color: 0xFFFFFF,
+              //   transparent: true,
+              //   opacity: 0,
+              // });
 
-              var meshSVG = new THREE.Mesh(geomSVG, materialSVG)
-              svgGroup.add(meshSVG);
+              //var meshSVG = new THREE.Mesh(geomSVG, materialSVG);
+
+              var borderMaterial = new THREE.LineBasicMaterial({color: 0x000000, linewidth: 3})
+              var lineGeometry = new THREE.EdgesGeometry(geomSVG, 15);
+              var bordermesh = new THREE.LineSegments(lineGeometry, borderMaterial);
+
+              //svgGroup.add(meshSVG);
+              svgGroup.add(bordermesh);
+              
             })
           })
-          svgGroup.rotation.x = - Math.PI / 2;
-          svgGroup.rotation.y =  - Math.PI;
-          svgGroup.rotation.z =  - Math.PI;
-          svgGroup.position.y = 1000;
-          scene.add(svgGroup);
           
+          var axesHelper = new THREE.AxesHelper(50000);
+          scene.add(axesHelper);
+
+          // svgGroup.rotation.x = - Math.PI / 2;
+          //svgGroup.rotation.x =  - Math.PI;
+          //svgGroup.rotation.z =  - Math.PI - 0.1;
+          svgGroup.rotateX(Math.PI / 2)
+          //svgGroup.rotateZ(THREE.Math.degToRad(8.5))
+          svgGroup.position.z = -850;
+          svgGroup.position.x = 16600;
+          // svgGroup.position.y = 1500;
+
+          scene.add(svgGroup);
+          svg.remove();
+
         })
 
       animate();
@@ -292,7 +296,8 @@ class Map extends React.Component {
 
       //controls.update();
     }
-    window.addEventListener('resize', onWindowResize, false)
+
+    window.addEventListener('resize', onWindowResize, false);
 
   }
 
@@ -310,7 +315,7 @@ class Map extends React.Component {
     return (
 
       <div>
-        <div id="Map" className="border border-primary rounded img-thumbnail" style={{ backgroundColor: "#7fcdff" }}></div>
+        <div id="Map" style={{ backgroundColor: "#7fcdff", display: "none"}}></div>
         <div className="" id="main_map" style={{ width: 100 + "%", height: 100 + "%" }}></div>
       </div>
     );
