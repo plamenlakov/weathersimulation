dc062cc182d826654ed5682f1df73528354ff7fc
Borislav Kiselkov <borislav@Borislavs-MacBook-Pro.local>
2020-06-01 13:00:45 +0200

    nz

--- resources/views/auth/account.blade.php
+++ resources/views/auth/account.blade.php
@@ -2,7 +2,12 @@
 
 @section('content')
 <div class='container shadow p-3 mb-5 mt-5 bg-white rounded' style='height: 650px'>
-    
+    <div class='text-right'>
+        {{Auth::user()->email}}
+    </div>
+    <div class="text-center">
+        <img class="rounded thumbnail m-3" src="" alt="profile pic">
+    </div>
 </div>
 
 @endsection
