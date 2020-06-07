7baeba56e55143b547d2f31ae6d8f6d803e247f0
BobbyPC <borislav.kiselkov@gmail.com>
2020-06-07 19:48:06 +0200

    homepage

--- resources/js/components/Stepper.js
+++ resources/js/components/Stepper.js
@@ -5,6 +5,12 @@ import StepLabel from '@material-ui/core/StepLabel';
 import StepContent from '@material-ui/core/StepContent';
 import Button from '@material-ui/core/Button';
 import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Typography from '@material-ui/core/Typography';
 
 class StepperTutorial extends React.Component {
 
@@ -17,86 +23,235 @@ class StepperTutorial extends React.Component {
 
     handleStepUp() {
         this.setState({
-            activeStep: this.state.activeStep++
+            activeStep: this.state.activeStep + 1
         })
     }
 
     handleStepDown() {
         this.setState({
-            activeStep: this.state.activeStep--
+            activeStep: this.state.activeStep - 1
+        })
+    }
+
+    handleReset() {
+        this.setState({
+            activeStep: 0
         })
     }
 
     render() {
+
+
         return (
             <div>
-                <Stepper activeStep={this.state.activeStep} orientation='vertical'>
-                    <Step key='1st step'>
-                        <StepLabel>1st step</StepLabel>
-                        <StepContent>
-                            Neshto da ima
+                <Stepper style={{ backgroundColor: "transparent" }} activeStep={this.state.activeStep} orientation='vertical'>
+                    <Step key='1st Step'>
+                        <StepLabel>Introduction</StepLabel>
+                        <StepContent className='text-center'>
+                            <h2>We have created a quick tutorial to have you started in our simulation!</h2>
+                            <div className='text-center'>
+                               
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={this.handleStepUp.bind(this)}
+                                >
+                                    Start
+                                </Button>
+                            </div>
                         </StepContent>
-                        <div>
-                            <Button
-                                disabled={this.state.activeStep === 0}
-                                onClick={this.handleStepDown.bind(this)}
-                                color='secondary'
-                            >
-                                Back
-                            </Button>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={this.handleStepUp.bind(this)}
-                            >
-                                {this.state.activeStep === 3 - 1 ? 'Finish' : 'Next'}
-                            </Button>
-                        </div>
                     </Step>
 
-                    <Step key='2nd step'>
+                    <Step key='1st Step'>
+                        <StepLabel>Changing main inputs</StepLabel>
                         <StepContent>
-                            Neshto da ima 2
+                            <Card>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="open controls"
+
+                                        image="images/main/OpenControls.gif"
+                                        title="Open controls"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Adding new values
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            Each value represents a percentage in the increase of a chosen sector. These percentages will take affect for each and every year for all the countries. This means that the same percentage will be added again and again each year. To avoid huge values consider inputting smaller numbers as shown in the example above.
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+
+                            </Card>
+                            <div className='float-right'>
+                                <Button
+                                    className='m-2'
+                                    disabled={this.state.activeStep === 0}
+                                    onClick={this.handleStepDown.bind(this)}
+                                    color='secondary'
+                                >
+                                    Back
+                            </Button>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={this.handleStepUp.bind(this)}
+                                >
+                                    Next
+                                </Button>
+                            </div>
                         </StepContent>
-                        <div>
-                            <Button
-                                disabled={this.state.activeStep === 0}
-                                onClick={this.handleStepDown.bind(this)}
-                                color='secondary'
-                            >
-                                Back
-                            </Button>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={this.handleStepUp.bind(this)}
-                            >
-                                {this.state.activeStep === 3 - 1 ? 'Finish' : 'Next'}
-                            </Button>
-                        </div>
                     </Step>
-                    
-                    <Step key='3rd step'>
+
+                    <Step key='2nd Step' >
+                        <StepLabel>Changing a single country input</StepLabel>
                         <StepContent>
-                            Neshto da ima 3
+                            <Card>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="change values single country"
+
+                                        image="images/main/OpenOneCountry.gif"
+                                        title="change values single country"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Single country sector change
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            Each value represents a percentage in the increase of a chosen sector but for one country. Under the 'Political map' tab, you can find a 2D political map of Europe. You can change the values of all countries in the EU + UK. Again, the percentage is applied for each year of the simulation.
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+
+                            </Card>
+                            <div className='float-right'>
+                                <Button
+                                    className='m-2'
+                                    disabled={this.state.activeStep === 0}
+                                    onClick={this.handleStepDown.bind(this)}
+                                    color='secondary'
+                                >
+                                    Back
+                            </Button>
+                                <Button
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={this.handleStepUp.bind(this)}
+                                >
+                                    Next
+                                </Button>
+                            </div>
                         </StepContent>
-                        <div>
-                            <Button
-                                disabled={this.state.activeStep === 0}
-                                onClick={this.handleStepDown.bind(this)}
-                                color='secondary'
-                            >
-                                Back
-                            </Button>
-                            <Button
-                                variant="contained"
-                                color="primary"
-                                onClick={this.handleStepUp.bind(this)}
-                            >
-                                {this.state.activeStep === 3 - 1 ? 'Finish' : 'Next'}
-                            </Button>
-                        </div>
+
                     </Step>
+
+                    <Step key='Starting your simulation'>
+                        <StepLabel>Starting your simulation</StepLabel>
+                        <StepContent>
+                            <Card>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="open controls"
+
+                                        image="images/main/StartSimulation.gif"
+                                        title="Open controls"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Starting your simulation
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            When you have the desired inputs, the only thing left is to open the controls again and click on 'Create new simulation'.
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+
+                            </Card>
+                            <div className='float-right'>
+                                <Button
+                                    className='m-2'
+                                    disabled={this.state.activeStep === 0}
+                                    onClick={this.handleStepDown.bind(this)}
+                                    color='secondary'
+                                >
+                                    Back
+                            </Button>
+                                <Button
+
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={this.handleStepUp.bind(this)}
+                                >
+                                    Next
+                                </Button>
+                            </div>
+                        </StepContent>
+
+                    </Step>
+                    <Step key='Changing values on run time'>
+                        <StepLabel>Changing values on run time</StepLabel>
+                        <StepContent>
+                            <Card>
+                                <CardActionArea>
+                                    <CardMedia
+                                        component="img"
+                                        alt="open controls"
+
+                                        image="images/main/ChangeValuesOnRunTime.gif"
+                                        title="Open controls"
+                                    />
+                                    <CardContent>
+                                        <Typography gutterBottom variant="h5" component="h2">
+                                            Change inputs on run time
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary" component="p">
+                                            In order to change anything while the simulation is running you must pause it. Then, you are able to open the contols or any country you desire, and change the values. The changes will then be applied for the years ahead.
+                                        </Typography>
+                                    </CardContent>
+                                </CardActionArea>
+
+                            </Card>
+                            <div className='float-right'>
+                                <Button
+                                    className='m-2'
+                                    disabled={this.state.activeStep === 0}
+                                    onClick={this.handleStepDown.bind(this)}
+                                    color='secondary'
+                                >
+                                    Back
+                            </Button>
+                                <Button
+
+                                    variant="contained"
+                                    color="primary"
+                                    onClick={this.handleStepUp.bind(this)}
+                                >
+                                    Finish
+                                </Button>
+                            </div>
+                        </StepContent>
+
+                    </Step>
+                    <Step key='1st Step'>
+                        <StepLabel>Ready to go</StepLabel>
+                        <StepContent className='text-center'>
+                            <h2>You are all done!</h2>
+                            <div className='text-center'>
+                                <Button className='m-2' variant="contained" color="success" href="/simulation">
+                                    Create your own
+                            </Button>
+                                <Button onClick={this.handleReset.bind(this)} variant="contained" color='primary'>
+                                    Reset
+                            </Button>
+                            </div>
+                        </StepContent>
+                    </Step>
+                   
                 </Stepper>
             </div>
         )
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -34,78 +34,22 @@
     </div>
 
 </div>
-<div class="container">
+<div class="">
 
     <div class='m-4 text-justify'>
         <h3>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptas similique quia esse quisquam non repudiandae quod distinctio molestias id atque, iusto modi perferendis iste voluptate corrupti perspiciatis iure dignissimos fuga excepturi quos quidem nobis nulla tempora? Iure repellat adipisci assumenda veritatis itaque modi totam hic, molestias architecto. Cupiditate, provident tenetur architecto corrupti perferendis expedita, praesentium minima facilis magnam est voluptatem officia error consequatur quas iusto illo exercitationem, tempore repellendus! Corporis natus labore laborum inventore rerum, facere reprehenderit voluptates repellat fugit nostrum consequatur, exercitationem dolorem? Officia esse eaque reiciendis sed nisi culpa qui aperiam voluptatem, iusto quas consequatur voluptatibus ipsum quam modi enim totam? Deserunt dolorum accusantium incidunt recusandae ipsa eum, corporis magni consequuntur corrupti nobis velit tempora consequatur aliquid! Voluptatum ratione magnam officiis molestiae ipsum sequi, explicabo minus quod nesciunt qui consequatur tenetur iste nemo mollitia adipisci laudantium ea vel ipsa perferendis in alias pariatur. Veritatis dolore dolores illum sit porro ad at labore saepe totam, possimus voluptate ex debitis nesciunt. Voluptatibus dolore dolorum vel, expedita nostrum eaque dolorem iste illum quasi labore praesentium eveniet dolores cupiditate porro veritatis laborum repellendus ullam delectus nobis eum! Dignissimos pariatur repudiandae omnis nostrum sapiente obcaecati delectus non aut sint ullam aliquam, impedit accusantium.</h3>
     </div>
 </div>
-
-<div id="stepper"></div>
-
-<!-- <div class="m-3 alert alert-info" role="alert">
-    <h2 class="alert-heading">Make your own</h2>
+<div class="m-4 alert alert-primary" role="alert">
+    <h2 class="alert-heading">Create your own</h2>
     <hr>
-    <div class="row ">
-        <div class="col-md-4 align-self-center">
-            <div class="m-3 mt-5">
-                <div class="card">
-                    <img class="card-img-top" src="/images/main/NASA.png" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 text-center align-self-center ">
-            <img src="images/main/arrow_right.png" alt="arrow">
-        </div>
-        <div class="col-md-4">
-            <div class="m-3 mt-5">
-                <div class="card">
-                    <img class="card-img-top" src="/images/main/NASA.png" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+    <div class="w-75 mx-auto">
+
+        <div id="stepper"></div>
     </div>
-    <div class="row m-3">
-        <div class="col align-self-right">
-            <img class="float-right h-50" src="images/main/arrow_left.png" alt="arrow" style='transform: rotate(-90deg)'>
-        </div>
-
-    </div>
-    <div class="row">
-        <div class="col-md-4">
-            <div class="m-3 mt-5">
-                <div class="card">
-                    <img class="card-img-top" src="/images/main/NASA.png" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 text-center align-self-center">
-            <img src="images/main/arrow_left.png" alt="arrow">
-        </div>
-        <div class="col-md-4">
-            <div class="m-3 mt-5">
-                <div class="card">
-                    <img class="card-img-top" src="/images/main/NASA.png" alt="Card image cap">
-                    <div class="card-body">
-                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+</div>
 
 
-
-</div> -->
 
 
 
